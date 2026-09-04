c5d25c425d3a21529c8659e710de336dc664b523
Umra <umrakhan1510@gmail.com>
2026-09-04 15:41:07 +0530

    Initial SAATHI prototype

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>SAATHI</h1>
+      <p>Cooperative Gig Services Platform</p>
+      <button>Find a Service</button>
+    </div>
+  )
+}
+
+export default App
